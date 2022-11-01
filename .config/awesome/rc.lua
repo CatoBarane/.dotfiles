9badfb7105b0f8fe42212e0a3fcc774c34e4b36d
CatoBarane <baranecato98@gmail.com>
2022-11-01 15:20:11 +0100

    make changes to instance rules

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -499,6 +499,10 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     --{ rule = { class = "Firefox" },
     --  properties = { screen = 1, tag = "2" } },
+
+	{ rule = { instance = "Alacritty" },
+  	properties = { screen = "eDP-1" } }
+
 }
 -- }}}
 
